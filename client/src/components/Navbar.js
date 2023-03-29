073227abcc4a0331dd9832b458589743cb7afbfd
barsu5489 <ebarsulai@gmail.com>
2023-03-29 19:43:40 +0300

    Navbar routes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <><nav>
+   <div className="navbar-logo">
+          <NavLink to="/">Wera</NavLink>
+        </div>
+        
+        <div className="navbar-links">
+          {/* <NavLink to="/auth">Currently working<br /> on REACT</NavLink> */}
+          <NavLink to="/jobs">My Jobs</NavLink>
+          <NavLink to="/categories">Categories</NavLink>
+          <NavLink to="/Postjob">Post job</NavLink>
+          <button className='btn-logout'>Log Out</button>
+        </div>
+      </nav>
+  </>
   )
 }
 
