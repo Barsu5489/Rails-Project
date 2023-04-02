ee8129f99e1efad8571fd9ba95fa40422d5f12ea
barsu5489 <ebarsulai@gmail.com>
2023-04-02 03:50:43 +0300

    home/landing page

--- client/src/components/UserHome.js
+++ client/src/components/UserHome.js
@@ -2,30 +2,32 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function UserHome() {
+  const user_id = localStorage.getItem('userId');
 
   const redirect = useNavigate()
   const userInfo = localStorage.getItem('userInfo')
   const userLastName = localStorage.getItem('userLastName')
+  console.log(userInfo)
 
  
-  console.log(userInfo)
+  console.log(user_id)
   return (
     <>
     <div className='homeContainer'>
     <div className='currentJob'>
-    {userInfo ? (<h1> <span style={{color:'Green'}}>{userInfo}</span>, <br />the Portfolio Picaso</h1>):(<><h1>Be captain of <br /> the Portfolio Ship <span className='access' onClick={()=>redirect('/login')}>Get a Job</span></h1> 
+    {userInfo ? (<h1> <span style={{color:'Green'}}>{userInfo}</span> <br />Discover</h1>):(<><h1>Create or<br />find opportunities <span className='access' onClick={()=>redirect('/login')}>Get a Job</span></h1> 
     
     </>
     )}
         
     </div>
     <div className='userImage'>
-        <img style={{height:'25vh'}} src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" srcSet="" />
+        <img style={{height:'25vh'}} src="https://images.unsplash.com/photo-1629709303904-06d564f237d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80" alt="" srcSet="" />
     </div>
     <div  className='userSummary'>
         
-        <p style={{marginTop:'80px'}}>You can now focus on what matters most.<br /> Gone are the days of strssing about your portfolio </p>
-        {userInfo ?(<h1><span style={{color:'green'}}>{userInfo}</span> <br /> {userLastName}</h1>):(<h1>Portfolio <br /> Picaso</h1>)}
+        <p style={{marginTop:'80px'}}>Simplify your job search and job posting with our platform. <br /> Join our community and find the ideal position.</p>
+        {userInfo ?(<h1><span style={{color:'green'}}>{userInfo}</span> <br /> {userLastName}</h1>):(<h1>Find jobs <br /> Post Jobs</h1>)}
         
     </div>
     </div>
